a7a46374589d6354325362e0bfe916b4c111795d
Vinicius <viniciusmartinsdacunha1@gmail.com>
2022-09-01 19:08:24 -0300

    tete

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+const logoHeader = document.querySelector('.logoHeader')
+const btnMenu = document.querySelector('.menuBar')
+const liMobile = document.querySelectorAll('li')
+const header = document.querySelector('header')
+const logo = document.querySelector('.logo')
+const body = document.querySelector('body')
+const nav = document.querySelector('nav')
+
+function toggleMenu() {
+  header.classList.toggle('activeMenu')
+  nav.classList.toggle('hide')
+  body.classList.toggle('overflow')
+}
+
+function onScroll() {
+  if (scrollY > 85) {
+    logo.classList.add('hide')
+    logoHeader.classList.add('topDown')
+    logoHeader.classList.remove('hide')
+  } else {
+    btnMenu.classList.add('leftRight')
+    logo.classList.remove('hide')
+    logoHeader.classList.add('hide')
+  }
+}
+
+btnMenu.addEventListener('click', toggleMenu)
