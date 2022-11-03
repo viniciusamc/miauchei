4ad76dd96544f48a04cfa36d42e941529c6b804c
Vinicius <vinicius@pop-os.localdomain>
2022-11-02 23:20:00 -0300

    animais perdidos

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
+const fecharPopup = document.querySelector('.close-window');
 const logoHeader = document.querySelector('.logoHeader')
+const anunciar = document.querySelector('.anunciar');
+const popup = document.querySelector('.pop-dialog')
 const btnMenu = document.querySelector('.menuBar')
 const liMobile = document.querySelectorAll('li')
 const header = document.querySelector('header')
@@ -6,10 +9,19 @@ const logo = document.querySelector('.logo')
 const body = document.querySelector('body')
 const nav = document.querySelector('nav')
 
+
 function toggleMenu() {
   header.classList.toggle('activeMenu')
   nav.classList.toggle('hide')
   body.classList.toggle('overflow')
+}
+
+function togglePopUp(){
+  popup.showModal();
+}
+
+function closeWindow(){
+  popup.close();
 }
 
 function onScroll() {
@@ -22,6 +34,8 @@ function onScroll() {
     logo.classList.remove('hide')
     logoHeader.classList.add('hide')
   }
-}
+};
 
-btnMenu.addEventListener('click', toggleMenu)
+btnMenu.addEventListener('click', toggleMenu);
+anunciar.addEventListener('click', togglePopUp);
+fecharPopup.addEventListener('click',closeWindow);
