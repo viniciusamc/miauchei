a6eccf90fa9389f9f671555c4f988f18edb59a81
vinicius <viniciusmartinsdacunha1@gmail.com>
2022-11-19 15:31:08 -0300

    header finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,22 @@
 const fecharPopup = document.querySelector('.close-window');
 const logoHeader = document.querySelector('.logoHeader')
+const btnMobile = document.querySelector('#btn-mobile')
 const anunciar = document.querySelector('.anunciar');
 const popup = document.querySelector('.pop-dialog')
 const liMobile = document.querySelectorAll('li')
 const header = document.querySelector('header')
 const logo = document.querySelector('.logo')
 const body = document.querySelector('body')
+const nav = document.querySelector('nav');
 
-const btnMobile = document.querySelector('#btn-mobile')
-const nav = document.querySelector('nav')
+const toggleMenuProfile = document.querySelector('.menu');
+const userPic = document.querySelector(".profile img")
+
+function handleMenu(){
+  toggleMenuProfile.classList.toggle("active")
+}
+
+userPic.addEventListener('click', handleMenu)
 
 function toggleMenu(){
   nav.classList.toggle("active");
