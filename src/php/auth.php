747de8740b612215a4f2a4967a65e62f1338a6d5
vinicius <viniciusmartinsdacunha1@gmail.com>
2022-12-12 19:31:21 -0300

    ..

--- src/php/auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION["nome"])){
-header("Location: login.php");
-exit(); }
-?>
--- src/php/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
- $conn = mysqli_connect("localhost","root","","miauchei")
-          or die("couldn't connect to database");
-
-
-?>
